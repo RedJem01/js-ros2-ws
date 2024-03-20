e1e01ea42a0f9f34613beb7053f59618b712a102
Jem <79197456+RedJem01@users.noreply.github.com>
2024-03-20 12:52:01 +0000

    Add laser scan stuff

--- src/colourSlicing.py
+++ src/colourSlicing.py
@@ -6,6 +6,7 @@
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError # Package to convert between ROS and OpenCV Images
 import cv2
@@ -15,72 +16,39 @@ class ColourContours(Node):
     def __init__(self):
         super().__init__('colour_contours')
         self.create_subscription(Image, '/limo/depth_camera_link/image_raw', self.camera_callback, 1)
+        self.create_publisher(String, '/msgs', 1)
 
         self.br = CvBridge()
 
     def camera_callback(self, data):
         #self.get_logger().info("camera_callback")
 
-        cv2.namedWindow("Image window", 1)
         try:
             cv_image = self.br.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        # using the BGR colour space, create a mask for everything
-        # that is in a certain range
-        bgr_thresh = cv2.inRange(cv_image,
-                                 np.array((200, 230, 230)),
-                                 np.array((255, 255, 255)))
-
-        # It often is better to use another colour space, that is
-        # less sensitive to illumination (brightness) changes.
-        # The HSV colour space is often a good choice. 
-        # So, we first change the colour space here...
         hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-
-        # ... and now let's create a binary (mask) image, looking for 
-        # any hue (range: 0-255), but for something brightly
-        # colours (high saturation: > 150)
-        hsv_thresh = cv2.inRange(hsv_img,
-                                 np.array((0, 150, 50)),
-                                 np.array((255, 0, 0)))
-
-        # just for the fun of it, print the mean value 
-        # of each HSV channel within the mask 
-        print(cv2.mean(hsv_img[:, :, 0], mask = hsv_thresh)[0])
-        print(cv2.mean(hsv_img[:, :, 1], mask = hsv_thresh)[0])
-        print(cv2.mean(hsv_img[:, :, 2], mask = hsv_thresh)[0])
-
-        # This is how we could find actual contours in
-        # the BGR image, but we won't do this now.
-        # _, bgr_contours, hierachy = cv2.findContours(
-        #     bgr_thresh.copy(),
-        #     cv2.RETR_TREE,
-        #     cv2.CHAIN_APPROX_SIMPLE)
-
-        # Instead find the contours in the mask generated from the
-        # HSV image.
-        hsv_contours, hierachy = cv2.findContours(
-            hsv_thresh.copy(),
-            cv2.RETR_TREE,
-            cv2.CHAIN_APPROX_SIMPLE)
         
-        # in hsv_contours we now have an array of individual
-        # closed contours (basically a polgon around the 
-        # blobs in the mask). Let's iterate over all those found 
-        # contours.
-        for c in hsv_contours:
-            # This allows to compute the area (in pixels) of a contour
-            a = cv2.contourArea(c)
-            # and if the area is big enough, we draw the outline
-            # of the contour (in blue)
-            if a > 100.0:
-                cv2.drawContours(cv_image, c, -1, (0, 0, 255), 10)
-        print('====')
+        bright = np.uint8([[[255,0,0 ]]])
+        hsv_bright = cv2.cvtColor(bright, cv2.COLOR_BGR2HSV)
+    
+        # preparing the mask to overlay 
+        mask = cv2.inRange(hsv_img, np.array((0, 150, 50)),
+                                 np.array((255, 255, 255))) 
+        
+        # The black region in the mask has the value of 0, 
+        # so when multiplied with original image removes all non-blue regions 
+        result = cv2.bitwise_and(cv_image, cv_image, mask = mask) 
 
         cv_image_small = cv2.resize(cv_image, (0,0), fx=0.4, fy=0.4) # reduce image size
-        cv2.imshow("Image window", cv_image_small)
+        mask_small = cv2.resize(mask, (0,0), fx=0.4, fy=0.4)
+        result_small = cv2.resize(result, (0,0), fx=0.4, fy=0.4)
+        
+        print(np.mean(cv_image_small))
+        cv2.imshow('frame', cv_image_small)
+        cv2.imshow('mask', mask_small)
+        cv2.imshow('result', result_small)
         cv2.waitKey(1)
 
 def main(args=None):
